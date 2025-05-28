2c2b3777fc73cedbd2a11c12042fe1336d0c1118
chandan12-jpg <cyberkingbubble1234@gmail.com>
2025-05-28 15:39:25 +0530

    Initial commite

---
+++ challenges_lec2.js
@@ -0,0 +1,14 @@
+let numberguess = 3;
+let pizzaSize;
+if (numberguess <= 2)
+{
+    console.log("small");
+}
+else if (numberguess >= 2 && numberguess <= 5)
+{
+    console.log("Medium");
+}
+else
+{
+    console.log("Large");
+}
